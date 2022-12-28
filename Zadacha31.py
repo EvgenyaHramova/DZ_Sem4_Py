30949dcdd9cbc886f3d128c9290cd6273ed141f9
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-12-28 22:09:29 +0400

    file Z31

--- Zadacha31.py
+++ Zadacha31.py
@@ -5,7 +5,7 @@ num = int(input("Задайте число: "))
 list = []
 i = 2
 while i <= num:
-    if num % i == 0:   # and (1 < i < num)
+    if num % i == 0:     # and (1 < i < num)
         list.append(i)
         num = num // i  #num//=i
     i +=1
