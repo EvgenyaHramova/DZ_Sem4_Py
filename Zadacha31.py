ee74b7ef910fe8277a34b9057a17f6e2ec276d1a
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-12-28 21:24:06 +0400

    file Z31

--- Zadacha31.py
+++ Zadacha31.py
@@ -5,9 +5,9 @@ num = int(input("Задайте число: "))
 list = []
 i = 2
 while i <= num:
-    if num % i == 0: # and (1 < i < num)
+    if num % i == 0:   # and (1 < i < num)
         list.append(i)
-        num = num / i  #num//=i
+        num = num // i  #num//=i
     i +=1
 
-print(list)
+print('Список множителей числа: ', list)
